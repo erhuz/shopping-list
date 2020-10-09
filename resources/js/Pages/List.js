c452e1c4d0c08e9fcccfb9e5c9f0207dfee0d8be
alexjohnsv <alexjohnsv@gmail.com>
2020-10-09 16:24:06 +0200

    partial reload

--- resources/js/Pages/List.js
+++ resources/js/Pages/List.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppLayout from './../Layouts/AppLayout'
 import ItemList from './../Components/ItemList'
 import { Inertia } from '@inertiajs/inertia'
 
 export default function List(props) {
 
-    const [items, setItems] = useState([...props.items]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            Inertia.reload({ only: ['items']});
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);
 
     const handleChange = (item, event) => {
-        const itemIndex = items.findIndex(element => element.id === item.id);
+        const itemIndex = props.items.findIndex(element => element.id === item.id);
 
-        const updatedItems = [...items];
-        updatedItems[itemIndex].completed = !item.completed;
-
-        const updatedItem = updatedItems[itemIndex];
-
-        setItems(updatedItems);
+        const updatedItem = props.items[itemIndex];
+        updatedItem.completed = !item.completed;
 
         Inertia.put(`/api/list-item/${updatedItem.id}`, updatedItem);
     };
 
     return (
         <AppLayout title="List">
-            <ItemList name={props.name} items={items} handleChange={handleChange} />
+            <ItemList name={props.name} items={props.items} handleChange={handleChange} />
         </AppLayout>
     )
 }
