c5e3a77962998af9cf7646e685387bae2043e4ab
Benjamin Fransson <benjamin@cafcon.se>
2020-10-10 20:29:59 +0200

    Added autoFocus attribute to CreateItemForm.

--- resources/js/Components/CreateItemForm.js
+++ resources/js/Components/CreateItemForm.js
@@ -27,6 +27,7 @@ export default function GroceryInput ({id, createItem}) {
                 onKeyPress={(event) => {handleSubmit(event)}}
                 className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                 id="street_address"
+                autoFocus={true}
                 placeholder="Mjölk, kaffe, tv..."
             />
         </div>
