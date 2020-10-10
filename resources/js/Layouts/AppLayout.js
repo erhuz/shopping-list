712eafe1454fe472154e1dafa626ace613ec5401
Benjamin Fransson <benjamin@cafcon.se>
2020-10-10 19:48:11 +0200

    Fixed broken strokeWidth

--- resources/js/Layouts/AppLayout.js
+++ resources/js/Layouts/AppLayout.js
@@ -24,7 +24,7 @@ export default function AppLayout({ title, children }) {
                                 onClick={() => setSidebarIsOpen(false)}>
                                 <svg className="h-6 w-6 text-white" stroke="currentColor" fill="none"
                                      viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2"
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                           d="M6 18L18 6M6 6l12 12"/>
                                 </svg>
                             </button>
@@ -57,7 +57,7 @@ export default function AppLayout({ title, children }) {
                         onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
                         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2"
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M4 6h16M4 12h16M4 18h16"/>
                         </svg>
                     </button>
